0103887ca4fb7e017021f8cadf26a58b5f7e0db9
Junyi LI <junyi4000@gmail.com>
2025-02-17 08:28:05 +0100

    fix: scale and light of computer canvas

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,15 +3,17 @@ import ComputersCanvas from "../components/canvas/computer";
 
 const Home = () => {
   return (
-    <div className="about-container p-4 text-center">
-      <h1 className="text-2xl font-bold mb-4">About Me</h1>
-      <p className="text-gray-700">
-        Hi, I'm Junyi Li, a passionate software engineer specializing in AI and
-        Data Science. I love solving complex problems and creating impactful
-        solutions. Welcome to my portfolio!
-      </p>
+    <section className="relative w-full h-screen mx-auto">
+      <div className="p-4 text-center">
+        <h1 className="text-2xl font-bold mb-4">About Me</h1>
+        <p className="text-gray-700">
+          Hi, I'm Junyi Li, a passionate software engineer specializing in AI
+          and Data Science. I love solving complex problems and creating
+          impactful solutions. Welcome to my portfolio!
+        </p>
+      </div>
       <ComputersCanvas />
-    </div>
+    </section>
   );
 };
 
